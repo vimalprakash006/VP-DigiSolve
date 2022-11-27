c0b5f4ac4992cf027be0bd4066c8d6e309567278
vimalprakash006 <97742230+vimalprakash006@users.noreply.github.com>
2022-11-28 00:59:22 +0530

    Adding all files of VP-Digisolve

---
+++ Resourses/js/script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $(".about-section").waypoint(function(direction){
+//         $("nav").addclass("sticky-nav");
+//     })
+// });
+
+// let get_ready = alert ("Get ready for an eye-treat!");
+// let fill_up = prompt("Are you in?");
+// if (fill_up = yes) {
+//     alert ("You can go ahead!");
+// }
+// else {
+//     alert ("Get lost!");
+// }
